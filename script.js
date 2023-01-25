763875a06582d100120cd847a20d436bebd75712
Nielsington <declercqniels@hotmail.com>
2023-01-25 16:29:53 +0100

    Add clearing 5 day forecast
    - Fix footer jumping up and down
    - Fix 5 day forecast displaying wrong city name
    - Fix state 'undefined'
    - Add dates to 5 day forecast
    - Add background image based on city search (API)

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-//TODO: Remove 5day forecast on new search
-//TODO: Add dates to 5 day forecast
-
-
-
 const inputCity = document.querySelector('input');
 const keyGeo = 'secret';
 const keyForecast = 'secret';
@@ -18,6 +13,8 @@ const lonlatParagraph = document.querySelector('.lonlat');
 const description = document.querySelector('.description');
 const forecast = document.querySelector('.forecast-container');
 const footer = document.querySelector('footer');
+const input = document.querySelector('input');
+let lon, lat;
 
 weatherContainer.style.display = 'none';
 
@@ -33,47 +30,46 @@ inputCity.addEventListener('blur', function(){
     }
 });
 
-
-const input = document.querySelector('input');
-
 inputCity.addEventListener('keyup', function(e){
 
     if (e.key === 'Enter') {
+        forecast.innerHTML = '';
         let city = input.value;
-        let lon, lat;
-
         weatherContainer.style.display = '';
         inputContainer.style.marginTop = '2rem';
-        footer.style.transform = "translateY(120%)";
-        
+        footer.style.position = 'static';
 
         //Get lon, lat, country and display them
         fetch('http://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=1&appid=' + keyGeo)
         .then(response => response.json())
-        .then(data => {
-            lon = data[0].lon;
-            lat = data[0].lat;
-        
-            cityParagraph.innerHTML = data[0].name + ', '+ data[0].state + ', ' + data[0].country;  
+        .then(data1 => {
+            lon = data1[0].lon;
+            lat = data1[0].lat;
+            
+            if(data1[0].state === undefined){
+                cityParagraph.innerHTML = data1[0].name + ', ' + data1[0].country + '<br><br>';
+            } else{
+                cityParagraph.innerHTML = data1[0].name + ', '+ data1[0].state + ', ' + data1[0].country; 
+            }
+
             lonlatParagraph.innerHTML = '<b>GPS coordinates:</b><br><br>   ' + lon + ', ' + lat ;
 
             // Get all the other info and display them
-            fetch('http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=metric&appid=' + keyForecast)
+            return fetch('http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=metric&appid=' + keyForecast)
             .then(response => response.json())
-            .then(data => {
-                degreeParagraph.innerHTML = '<b>' + Math.round(data.list[0].main.temp) + '°C </b>';
-                windParagraph.innerHTML = '<b>Wind speed:</b><br><br>  ' + data.list[0].wind.speed + ' meter/sec';
-                description.innerHTML = data.list[0].weather[0].description + ', captain.';
+            .then(data2 => {
+                degreeParagraph.innerHTML = '<b>' + Math.round(data2.list[0].main.temp) + '°C </b>';
+                windParagraph.innerHTML = '<b>Wind speed:</b><br><br>  ' + data2.list[0].wind.speed + ' meter/sec';
+                description.innerHTML = data2.list[0].weather[0].description + ', captain.';
 
                 //Weather icon
-                weatherIcon.src = 'http://openweathermap.org/img/wn/'+ data.list[0].weather[0].icon + '@2x.png'
-                console.log(data);
+                weatherIcon.src = 'http://openweathermap.org/img/wn/'+ data2.list[0].weather[0].icon + '@2x.png'
 
                 // Create forecast
-                // Calculate time (1 day = (7x3hours), 5 days = (35x3hours))
+                // Calculate time (1 day = ((8-1)x3hours), 5 days = (35x3hours))
                 let forecastDates = [];
                 for (let i = 0; i<36; i++){
-                    let dt = data.list[i].dt;
+                    let dt = data2.list[i].dt;
                     let date = new Date(dt*1000);
                     let hour = date.getUTCHours();
                     let minute = date.getUTCMinutes();
@@ -84,7 +80,6 @@ inputCity.addEventListener('keyup', function(e){
                     }
 
                 }
-                console.log(forecastDates);
 
                 // Add cards for 5 days
                 for (let forecastDate of forecastDates){
@@ -93,11 +88,14 @@ inputCity.addEventListener('keyup', function(e){
                     forecast.appendChild(forecastCard);
 
                     const weatherIcon = document.createElement('img');
-                    weatherIcon.src = 'http://openweathermap.org/img/wn/'+ data.list[forecastDate].weather[0].icon + '@2x.png';
+                    weatherIcon.src = 'http://openweathermap.org/img/wn/'+ data2.list[forecastDate].weather[0].icon + '@2x.png';
                     forecastCard.appendChild(weatherIcon);
 
                     const cityParagraph = document.createElement('h1');
                     forecastCard.appendChild(cityParagraph);
+
+                    const date = document.createElement('p');
+                    forecastCard.appendChild(date);
 
                     const forecastInfoContainer = document.createElement('div');
                     forecastCard.appendChild(forecastInfoContainer);
@@ -106,6 +104,7 @@ inputCity.addEventListener('keyup', function(e){
                     forecastInfoContainer.appendChild(degreeParagraph)
 
                     const description = document.createElement('p');
+                    description.classList.add('description');
                     forecastInfoContainer.appendChild(description);
 
                     const forecastCoord = document.createElement('div');
@@ -117,21 +116,44 @@ inputCity.addEventListener('keyup', function(e){
                     const lonlatParagraph = document.createElement('p');
                     forecastCoord.appendChild(lonlatParagraph);
                     
-                    cityParagraph.innerHTML = data.city.name + ', ' + data.city.country + '<br><br>';
-                    degreeParagraph.innerHTML = '<b>' + Math.round(data.list[forecastDate].main.temp) + '°C </b><br><br>';
-                    description.innerHTML = data.list[forecastDate].weather[0].description + ', captain.<br><br>';
-                    windParagraph.innerHTML = '<b>Wind speed:</b><br><br>  ' + data.list[forecastDate].wind.speed + ' meter/sec<br><br>';
-                    lonlatParagraph.innerHTML = '<b>GPS coordinates:</b><br><br>   ' + data.city.coord.lon + ', ' + data.city.coord.lat ;
+                    //TODO:
+                    if(data1[0].state === undefined){
+                        cityParagraph.innerHTML = data1[0].name + ', ' + data1[0].country + '<br><br>';
+                    } else{
+                        cityParagraph.innerHTML = data1[0].name + ', '+ data1[0].state + ', ' + data1[0].country + '<br><br>';
+                    }
+
+                    let transformDate = new Date(data2.list[forecastDate].dt*1000);
+                    
+                    const getDate = transformDate.toLocaleDateString('en-'+ data2.city.country, {
+                        weekday: "short",
+                        day: "2-digit",
+                        month: "long",
+                        year: "numeric"
+                    });
+                    date.innerHTML = getDate + '<br><br>';
+
+                    degreeParagraph.innerHTML = '<b>' + Math.round(data2.list[forecastDate].main.temp) + '°C </b><br><br>';
+                    description.innerHTML = data2.list[forecastDate].weather[0].description + ', captain.<br><br>';
+                    windParagraph.innerHTML = '<b>Wind speed:</b><br><br>  ' + data2.list[forecastDate].wind.speed + ' meter/sec<br><br>';
+                    lonlatParagraph.innerHTML = '<b>GPS coordinates:</b><br><br>   ' + data2.city.coord.lon + ', ' + data2.city.coord.lat ;
                     lonlatParagraph.style.marginBottom = '2rem';
                 }
             });
+        });
+        let unsplashAPI = "https://api.unsplash.com/search/photos/?client_id=8uQObWzQ5xcpl0fCOyaL6DV1JAEyMNsZi6pYShPrOFw&query=" + city;
+        fetch(unsplashAPI)
+        .then(response => response.json())
+        .then(photo => {
+            let picture = photo.results[0].urls.full;
+            document.body.style.background = `url(${picture})`;
+            document.body.style.backgroundSize = 'cover';
+            document.body.style.backgroundRepeat = 'no-repeat';
         });
     }
 });
 
 //Add copyright
-
 let copyright = document.querySelector("footer");
-
 copyright.innerHTML = "&copy; 2023 - "+new Date().getFullYear()+" The Weather App - All Rights Reserved.";
 copyright.innerHTML += "<br/>Last Updated : "+document.lastModified;
